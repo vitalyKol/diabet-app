58e85303fdb99f6f6c35b3446adaccb14fef9f3d
vitalyKol <vental-kvo@yandex.ru>
2022-05-31 11:09:41 +0300

    add simple routing

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
 error_reporting( E_ALL );
-header("Location: /calendarshow.php");
+//header("Location: /calendarshow.php");
 
-echo "Hello world";
+$phpself = $_SERVER["PHP_SELF"];
+$phpself = substr($phpself, 0, -9);
+
+$dirlength = strlen($phpself);
+$uri = $_SERVER["REQUEST_URI"];
+$uri = substr($uri, $dirlength);
+echo "<pre>";
+print_r($uri);
+echo "</pre>";
+
+$pages = [
+    "" => "calendarshow.php",
+    "calendar" => "calendarshow.php",
+    "day" => "dayshow.php"
+         ];
+
+//if(isset($pages[$uri])){
+//    require $pages[$uri];
+//}else{
+//    require '404.php';
+//}
+if(isset($pages[$uri])){
+    header("Location: /$pages[$uri]");
+}else{
+    header("Location: /404.php");
+}
