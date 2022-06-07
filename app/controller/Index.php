bbaff4f2daed0b4db4c82ff422d98f3dfe957aaa
vitalyKol <vental-kvo@yandex.ru>
2022-06-07 11:33:17 +0300

    clear index files

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -7,12 +7,8 @@ use system\Controller;
 class Index extends Controller
 {
     public function index(){
-//        echo "<pre>";
-//        print_r($_SERVER);
-//        echo "</pre>";
     }
 
     public function error404(){
-
     }
 }
