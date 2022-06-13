7675df46f85e2d4b069682bacec2b5e196668639
vitalyKol <vental-kvo@yandex.ru>
2022-06-13 13:53:01 +0300

    add tooltips for login page and register page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,20 @@
 
 let formSugar = document.getElementById('formSugar');
 let formMeal = document.getElementById('formMealTime');
-formSugar.addEventListener('show.bs.collapse', event => {
-    formMeal.classList.remove('show');
-})
-formMeal.addEventListener('show.bs.collapse', event => {
-    formSugar.classList.remove('show');
+if(formSugar){
+    formSugar.addEventListener('show.bs.collapse', event => {
+        formMeal.classList.remove('show');
+    })
+}
+if(formMeal){
+    formMeal.addEventListener('show.bs.collapse', event => {
+        formSugar.classList.remove('show');
+    })
+}
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl, {
+        container: document.querySelector('#tooltipContainer')
+    });
 })
